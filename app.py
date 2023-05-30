b749c77f08243a3376bc7fcaed3b4e1e2092d4c2
Piyush Jeena <56345075+pjeena@users.noreply.github.com>
2023-05-30 12:24:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ file_names = sorted(glob.glob(dir_input_to_model_data))
 
 st.write(file_names)    
 
-for file in file_names:
-        model = tf.keras.models.load_model(  os.path.join('models', file.split('.')[0].split('_test_')[1].replace(' ', '') + '.h5'  )  )
+#for file in file_names:
+model = tf.keras.models.load_model(  'models/[Paris]RivolixNicolasFlamel.h5'   )
         
         
 st.write(model)
