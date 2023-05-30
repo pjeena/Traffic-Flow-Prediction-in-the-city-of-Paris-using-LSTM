bf0a03badd43702760bc7df89b65332b1683eba0
Piyush Jeena <56345075+pjeena@users.noreply.github.com>
2023-05-30 12:48:50 +0200

    Update 01_🎈_Main_page.py

--- 01_🎈_Main_page.py
+++ 01_🎈_Main_page.py
@@ -125,7 +125,7 @@ with st.spinner('Getting live predictions from the models.....'):
 
 
 with st.sidebar:
-    st.warning('*Hover over the circles to see the junction name, corresponding traffic and change in % from prior hr*', icon="📌")
+    st.warning('*Hover over the circles to see the junction name, traffic and change in % from prior hr*', icon="📌")
     st.sidebar.markdown("**:red[*Scroll the main page below to see predictions for the 8 junctions in Paris*]**")
 
 
