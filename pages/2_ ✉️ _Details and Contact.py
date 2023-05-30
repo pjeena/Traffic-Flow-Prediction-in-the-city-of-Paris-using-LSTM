e75d51333bd2674ce6c70200c55c9c9c07afa211
Piyush Jeena <56345075+pjeena@users.noreply.github.com>
2023-05-30 21:52:55 +0200

    Update 2_ ✉️ _Details and Contact.py

--- pages/2_ ✉️ _Details and Contact.py
+++ pages/2_ ✉️ _Details and Contact.py
@@ -8,9 +8,6 @@ st.write(
 )
 
 st.code(
-    'This app forecasts the live traffic for the next 3 hours \n
-    in the famous streets of Paris. Additionally, it also \n
-    provides statistics for the historial traffic data such as distribution \n
-    of traffic among 2 wheelers and 4 wheelers, most busiest junction etc.'
+    'This app forecasts the live traffic for the next 3 hours in the famous streets of Paris. Additionally, it also provides statistics for the historial traffic data such as distribution of traffic among 2 wheelers and 4 wheelers, most busiest junction etc.'
 
 )
